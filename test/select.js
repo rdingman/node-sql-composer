3620f76d14bb216d718afbb9e02bc1ed6425cf2a
Ryan Dingman <rdingman@gmail.com>
2013-06-23 10:52:20 -0700

    Add limit clause support and associated test.

--- test/select.js
+++ test/select.js
@@ -9,10 +9,17 @@ exports.select = {
         test.done();
     },
 
-    "Select with order by": function (test) {
+    "Order by": function (test) {
         var statement = sql.select([ 'col1', 'col2', 'col3' ]).from({'table1' : 't1'}).orderBy({ 'col1' : null, 'col2' : 'desc' });
         var query = statement.toQuery();
         test.equal(query.sql, 'select col1, col2, col3 from table1 t1 order by col1, col2 desc');
+        test.done();
+    },
+
+    "Limit clause": function (test) {
+        var statement = sql.select([ 'col1', 'col2', 'col3' ]).from({'table1' : 't1'}).limit(100, 5);
+        var query = statement.toQuery();
+        test.equal(query.sql, 'select col1, col2, col3 from table1 t1 limit 100 offset 5');
         test.done();
     }
 }
