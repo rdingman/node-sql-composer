bb3381a74c7d7bb0c86a711490b606a859c8d57e
Ryan Dingman <rdingman@gmail.com>
2013-06-27 14:43:03 -0700

    Added insert statement generation and associated tests.

--- test/insert.js
+++ test/insert.js
@@ -3,6 +3,10 @@ var sql = require('../lib');
 
 exports.insert = {
     "Basic insert" : function (test) {
+        var statement = sql.insert().into('table1').values({ 'col1' : 123, 'col2' : 'Some text', 'col3' : sql.identifier('abc'),'col4' : true });
+        var query = statement.toQuery();
+        test.equal(query.sql, 'insert into table1 (col1, col2, col3, col4) values (?, ?, abc, true)');
+        test.deepEqual(query.values, [ 123, 'Some text' ]);
         test.done();
     },
 
