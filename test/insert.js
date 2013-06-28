5e00c688f43adf67ea63bce948043751106fab91
Ryan Dingman <rdingman@gmail.com>
2013-06-28 13:39:49 -0700

    Allow nulls as values when building an insert statement.

--- test/insert.js
+++ test/insert.js
@@ -3,9 +3,9 @@ var sql = require('../lib');
 
 exports.insert = {
     "Basic insert" : function (test) {
-        var statement = sql.insert().into('table1').values({ 'col1' : 123, 'col2' : 'Some text', 'col3' : sql.identifier('abc'),'col4' : true });
+        var statement = sql.insert().into('table1').values({ 'col1' : 123, 'col2' : 'Some text', 'col3' : sql.identifier('abc'), 'col4' : true, 'col5' : null });
         var query = statement.toQuery();
-        test.equal(query.sql, 'insert into table1 (col1, col2, col3, col4) values (?, ?, abc, true)');
+        test.equal(query.sql, 'insert into table1 (col1, col2, col3, col4, col5) values (?, ?, abc, true, null)');
         test.deepEqual(query.values, [ 123, 'Some text' ]);
         test.done();
     },
