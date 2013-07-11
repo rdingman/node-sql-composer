1838777195e2923a8ffea7a8a5cde7e86d9ab441
Ryan Dingman <rdingman@gmail.com>
2013-07-11 12:40:34 -0700

    Fixed a bug where 0 was being converted to null

--- test/insert.js
+++ test/insert.js
@@ -3,10 +3,10 @@ var sql = require('../lib');
 
 exports.insert = {
     "Basic insert" : function (test) {
-        var statement = sql.insert().into('table1').values({ 'col1' : 123, 'col2' : 'Some text', 'col3' : sql.identifier('abc'), 'col4' : true, 'col5' : null });
+        var statement = sql.insert().into('table1').values({ 'col1' : 123, 'col2' : 'Some text', 'col3' : sql.identifier('abc'), 'col4' : true, 'col5' : null, 'col6' : 0 });
         var query = statement.toQuery();
-        test.equal(query.sql, 'insert into table1 (col1, col2, col3, col4, col5) values (?, ?, abc, true, null)');
-        test.deepEqual(query.values, [ 123, 'Some text' ]);
+        test.equal(query.sql, 'insert into table1 (col1, col2, col3, col4, col5, col6) values (?, ?, abc, true, null, ?)');
+        test.deepEqual(query.values, [ 123, 'Some text', 0 ]);
         test.done();
     },
 
