aa2ad543462242e815964353fc92874554950f2a
Ryan Dingman <rdingman@gmail.com>
2013-07-22 11:38:36 -0700

    Add the ability to union select statements together.

---
+++ test/union.js
@@ -0,0 +1,14 @@
+
+var sql = require('../lib');
+
+exports.select = {
+    "Basic union" : function (test) {
+        var statement1 = sql.select([ 'col1', 'col2', 'col3' ]).from({'table1' : 't1'});
+        var statement2 = sql.select([ 'col1', 'col2', 'col3' ]).from({'table2' : 't2'});
+
+        var query = statement1.union(statement2).toQuery();
+        test.equal(query.sql, 'select col1, col2, col3 from table1 t1 union select col1, col2, col3 from table2 t2');
+        test.done();
+    }
+};
+
