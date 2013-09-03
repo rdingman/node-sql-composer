5698939712ef79eb52d00f84e41e206d637ba62e
Ryan Dingman <rdingman@gmail.com>
2013-09-03 14:12:47 -0700

    Fix bug where 0 was turned into null during updates.

--- test/update.js
+++ test/update.js
@@ -11,9 +11,9 @@ exports.insert = {
     },
 
     "Update with where clause" : function (test) {
-        var statement = sql.update('table1').set({ 'col1' : 123, 'col2' : 'Some text', 'col3' : sql.identifier('abc'), 'col4' : true, 'col5' : null }).where({ col1: 123, col2: '456'});
+        var statement = sql.update('table1').set({ 'col1' : 0, 'col2' : 'Some text', 'col3' : sql.identifier('abc'), 'col4' : true, 'col5' : null }).where({ col1: 123, col2: '456'});
         var query = statement.toQuery();
-        test.equal(query.sql, 'update table1 set col1 = 123, col2 = ?, col3 = abc, col4 = true, col5 = null where col1 = 123 and col2 = ?');
+        test.equal(query.sql, 'update table1 set col1 = 0, col2 = ?, col3 = abc, col4 = true, col5 = null where col1 = 123 and col2 = ?');
         test.deepEqual(query.values, [ 'Some text', '456' ]);
         test.done();
     }
